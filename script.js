78468e2b50bb8fb477ad467bd8208ea21868eb7a
Hannah Robinson <hannah.robinson.git@gmail.com>
2020-12-21 18:58:53 +1300

    Use for-loop to create flying books programmatically

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const clickMe = document.querySelector('.click-me');
 const container = document.querySelector('.container');
 const text = document.querySelector('h1');
 
-const bookEmoji = {
-  0: "📕",
-  1: "📘",
-  2: "📖",
-  3: "📗",
-  4: "📙",
-  5: "📓",
-  6: "📒",
-  7: "📔"
-};
-
 bookBtn.addEventListener('click', makeBooksFly);
 
 function makeBooksFly() {
@@ -22,71 +11,41 @@ function makeBooksFly() {
   bookBtn.remove();
   text.style.color = 'rgb(184, 184, 184)';
 
-  // Add for loop to create books flying left
-  
-  const book1 = document.createElement('div');
-  const book2 = document.createElement('div');
-  const book3 = document.createElement('div');
-  const book4 = document.createElement('div');
+  // Add for-loop to create flying books
 
-  book1.classList.add('book', 'book-1');
-  // book1.innerHTML = '&#128210;';
-  container.appendChild(book1);
+  for (let i = 0; i < 8; i++) {
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (i));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (8 + i));
+      container.appendChild(bookMirror);
+    }, (200 * i))
 
-  setTimeout(() => {
-    book2.classList.add('book', 'book-2');
-    // book2.innerHTML = '&#128217;';
-    container.appendChild(book2);
-  }, 100)
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (i));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (8 + i));
+      container.appendChild(bookMirror);
+    }, (250 * i))
 
-  setTimeout(() => {
-    book3.classList.add('book', 'book-1');
-    // book3.innerHTML = '&#128216;';
-    container.appendChild(book3);
-  }, 150)
-
-  setTimeout(() => {
-    book2.classList.add('book', 'book-1');
-    // book2.innerHTML = '&#128217;';
-    container.appendChild(book2);
-  }, 200)
-
-  setTimeout(() => {
-    book4.classList.add('book', 'book-1');
-    // book4.innerHTML = '&#128215;';
-    container.appendChild(book4);
-  }, 300)
-
-  setTimeout(() => {
-    book2.classList.add('book', 'book-2');
-    // book2.innerHTML = '&#128217;';
-    container.appendChild(book2);
-  }, 400)
-
-  setTimeout(() => {
-    book3.classList.add('book', 'book-1');
-    // book3.innerHTML = '&#128216;';
-    container.appendChild(book3);
-  }, 500)
-
-  setTimeout(() => {
-    book2.classList.add('book', 'book-2');
-    // book2.innerHTML = '&#128217;';
-    container.appendChild(book2);
-  }, 600)
-
-  setTimeout(() => {
-    book4.classList.add('book', 'book-1');
-    // book4.innerHTML = '&#128215;';
-    container.appendChild(book4);
-  }, 700)
-  
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (i));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (8 + i));
+      container.appendChild(bookMirror);
+    }, (300 * i))
+  }
 
   setTimeout(() => {
     text.innerHTML = 'This app <em>had</em> books.';
-  }, 3000);
-
-  
+    text.style.color = '#000';
+  }, 10000);  
 }
 
 
