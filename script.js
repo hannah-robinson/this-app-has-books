41c1f6931e42f95ffe6158c9e31a49e1f0853eba
Hannah Robinson <hannah.robinson.git@gmail.com>
2020-12-21 22:12:30 +1300

    Improve flying books' animation

--- script.js
+++ script.js
@@ -14,38 +14,75 @@ function makeBooksFly() {
   // Add for-loop to create flying books
 
   for (let i = 0; i < 8; i++) {
+    let randomNumber = Math.floor(Math.random() * 8);
     setTimeout(() => {
       const book = document.createElement('div');
-      book.classList.add('book', 'book-' + (i));
+      book.classList.add('book', 'book-' + (randomNumber));
       container.appendChild(book);
       const bookMirror = document.createElement('div');
-      bookMirror.classList.add('book', 'book-' + (8 + i));
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
       container.appendChild(bookMirror);
     }, (200 * i))
 
     setTimeout(() => {
       const book = document.createElement('div');
-      book.classList.add('book', 'book-' + (i));
+      book.classList.add('book', 'book-' + (randomNumber));
       container.appendChild(book);
       const bookMirror = document.createElement('div');
-      bookMirror.classList.add('book', 'book-' + (8 + i));
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
       container.appendChild(bookMirror);
     }, (250 * i))
 
     setTimeout(() => {
       const book = document.createElement('div');
-      book.classList.add('book', 'book-' + (i));
+      book.classList.add('book', 'book-' + (randomNumber));
       container.appendChild(book);
       const bookMirror = document.createElement('div');
-      bookMirror.classList.add('book', 'book-' + (8 + i));
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
       container.appendChild(bookMirror);
     }, (300 * i))
+
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (randomNumber));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
+      container.appendChild(bookMirror);
+    }, (360 * i))
+
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (randomNumber));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
+      container.appendChild(bookMirror);
+    }, (400 * i))
+
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (randomNumber));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
+      container.appendChild(bookMirror);
+    }, (500 * i))
+
+    setTimeout(() => {
+      const book = document.createElement('div');
+      book.classList.add('book', 'book-' + (randomNumber));
+      container.appendChild(book);
+      const bookMirror = document.createElement('div');
+      bookMirror.classList.add('book', 'book-' + (randomNumber + i));
+      container.appendChild(bookMirror);
+    }, (540 * i))
   }
 
   setTimeout(() => {
     text.innerHTML = 'This app <em>had</em> books.';
     text.style.color = '#000';
-  }, 10000);  
+  }, 7000);  
 }
 
 
